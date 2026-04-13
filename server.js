8a48a58a3996b04b2eb564d92418cce54db9b619
max-blizko-jpg <maxim.belousov@gmail.com>
2026-04-13 06:00:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const server = app.listen(port, () => {
 const peerServer = ExpressPeerServer(server, {
   path: '/',
   allow_discovery: true,
+  expire_timeout: 5000,    // Old connection expires after 5 seconds
+  alive_timeout: 60000,    // Keep-alive timeout
+  concurrent_limit: 5000,
+  cleanup_out_msgs: 1000,
 });
 
 app.use('/blizko', peerServer);
